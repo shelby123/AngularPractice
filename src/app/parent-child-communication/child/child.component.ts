2a64ed581a1f79a90dc8a7078c0a2ba5ca3aa810
Shelby mcgarrah <shelbymcgarrah96@gmail.com>
2019-05-22 12:44:28 -0500

    Communication from child to parent

--- src/app/parent-child-communication/child/child.component.ts
+++ src/app/parent-child-communication/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -12,6 +12,19 @@ export class ChildComponent implements OnInit {
   changestoChildInputString:number=0
   childInputStringPrev:string
   childInputStringNew:string
+
+  childDataProperty="This String is defined in the child component";
+
+  @Output() childMessageEvent:EventEmitter<string> = new EventEmitter<string>();
+
+  private _childTextAreaKey:string
+  get childTextAreaKey():string {
+    return this._childTextAreaKey
+  }
+  set childTextAreaKey(val:string) {
+    this._childTextAreaKey = val
+    this.childMessageEvent.emit(val);
+  }
 
   constructor() { }
 
--- src/app/parent-child-communication/parent-child-communication.component.ts
+++ src/app/parent-child-communication/parent-child-communication.component.ts
@@ -1,18 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { ChildComponent } from './child/child.component';
 
 @Component({
   selector: 'app-parent-child-communication',
   templateUrl: './parent-child-communication.component.html',
   styleUrls: ['./parent-child-communication.component.css']
 })
-export class ParentChildCommunicationComponent implements OnInit {
+export class ParentChildCommunicationComponent implements OnInit, AfterViewInit {
 
   parentTextAreaKey:String="hello";
 
+  childMessageString:String;
+
+  childChangingMessage:String;
+
+  @ViewChild(ChildComponent) childComponent:ChildComponent;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.childMessageString = this.childComponent.childDataProperty;
+  }
+
+  updateChildMessageDisplay(message:string) {
+    this.childChangingMessage=message;
   }
 
 }
