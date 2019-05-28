8aed2b10cd2a5046dea1c8c31cefbc210120f3cf
Shelby mcgarrah <shelbymcgarrah96@gmail.com>
2019-05-27 21:22:51 -0500

    finished multiple card forms

--- src/app/angular-routing/forms/forms.component.ts
+++ src/app/angular-routing/forms/forms.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 import { Model } from './data-model';
 
@@ -12,6 +12,8 @@ export class FormsComponent implements OnInit {
   exampleForm : FormGroup
   formArray:FormGroup[] = []
   dataModel = new Model("Sample Title", "Sample description")
+
+  @ViewChild("addButton") addButton : ElementRef
 
   descriptionControl =   new FormControl("", [
     Validators.required
@@ -33,47 +35,39 @@ export class FormsComponent implements OnInit {
     this.formArray = this.formArray.concat(this.buildFormGroup())
     this.formArray = this.formArray.concat(this.buildFormGroup())
 
-    this.formArray.forEach(item => {
-      item.get("repeatBoolean").valueChanges.subscribe(value =>{
-        if(value) {
-          item.get("numberRepeat").enable()
-          item.get("numberRepeat").setValidators(Validators.required)
-        } else {
-          item.get("numberRepeat").disable()
-          item.get("numberRepeat").clearValidators();
-        }
-        item.get("numberRepeat").updateValueAndValidity()
-       
-      })
-    })
-
-    
-    // this.exampleForm.get("repeatBoolean").valueChanges.subscribe(value =>{
-    //   if(value) {
-    //     this.exampleForm.get("numberRepeat").enable()
-    //     this.exampleForm.get("numberR epeat").setValidators(Validators.required)
-    //   } else {
-    //     this.exampleForm.get("numberRepeat").disable()
-    //     this.exampleForm.get("numberRepeat").clearValidators();
-    //   }
-    //   this.exampleForm.get("numberRepeat").updateValueAndValidity()
-     
-    // })
-
-   
   }
 
   addPin() {
     this.formArray = this.formArray.concat(this.buildFormGroup())
+    this.addButton.nativeElement.scrollIntoView()
   }
 
   buildFormGroup():FormGroup {
-    return this.fb.group({
+    let item = this.fb.group({
       title:["", [Validators.required ]],
       description: this.descriptionControl,
       repeatBoolean: false,
       numberRepeat: {value:"", disabled:true}
     })
+
+    item.get("repeatBoolean").valueChanges.subscribe(value =>{
+      if(value) {
+        item.get("numberRepeat").enable()
+        item.get("numberRepeat").setValidators(Validators.required)
+      } else {
+        item.get("numberRepeat").disable()
+        item.get("numberRepeat").clearValidators();
+      }
+      item.get("numberRepeat").updateValueAndValidity()
+     
+    })
+    return item
+  }
+  
+  remove(index:number) {
+    console.log("removing index " + index)
+    this.formArray.splice(index, 1)
+    console.log(this.formArray)
   }
 
 }
